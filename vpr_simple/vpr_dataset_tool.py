9c931534fb2a5b351a792247fac88477675db23f
Owen Claxton <claxtono@qut.edu.au>
2023-05-10 07:39:06 +1000

    updates: - added some help texts (not finished) - removed printer, returned use of self.print method - adjusted print messages and logging levels - fixed prep_netvlad/prep_hybridnet/pass_nns - fixed customise load/store in generate and load functions - fixed extend_dataset; added open_dataset - fixed load_dataset, _load; reintroduced _load - fixed swap - removed unused _extend

--- vpr_simple/vpr_dataset_tool.py
+++ vpr_simple/vpr_dataset_tool.py
@@ -7,13 +7,38 @@ import os
 import datetime
 from pathlib import Path
 from ..core.enum_tools import enum_name, enum_get
-from ..core.ros_tools import process_bag
-from ..vpr_classes import NetVLAD_Container, HybridNet_Container
+from ..core.ros_tools import process_bag, LogType, roslogger
+from ..core.helper_tools import formatException
 from ..core.file_system_tools import scan_directory
+from ..vpr_classes import NetVLAD_Container, HybridNet_Container
 from .vpr_helpers import *
 
 class VPRDatasetProcessor: # main ROS class
-    def __init__(self, dataset_params: dict, try_gen=True, init_netvlad=False, init_hybridnet=False, cuda=False, use_tqdm=False, autosave=False, printer=print):
+    def __init__(self, dataset_params: dict, try_gen=True, init_netvlad=False, init_hybridnet=False, cuda=False, use_tqdm=False, autosave=False, ros=True):
+        '''
+        Initialisation
+
+        Inputs:
+            dataset_params: dict type with following keys:
+                                - bag_name      [string]
+                                - npz_dbp       [string]
+                                - bag_dbp       [string]
+                                - odom_topic    [string]
+                                - img_topics    [string list]
+                                - sample_rate   [float]
+                                - ft_types      [string list]
+                                - img_dims      [int list]
+                                - filters       [string]
+            try_gen:        bool type {default: True}; whether or not to attempt generation if load fails
+            init_netvlad:   bool type {default: False}; whether or not to initialise netvlad (loads model into GPU)
+            init_hybridnet: bool type {default: False}; whether or not to initialise hybridnet (loads model into GPU)
+            cuda:           bool type {default: False}; whether or not to use CUDA for feature type/GPU acceleration
+            use_tqdm:       bool type {default: False}; whether or not to display extraction/loading statuses using tqdm
+            autosave:       bool type {default: False}; whether or not to automatically save any generated datasets
+            printer:        method type {default: print}; what function to use to display logging messages
+        Returns:
+            self
+        '''
         self.dataset_ready  = False
         self.cuda           = cuda
         self.use_tqdm       = use_tqdm
@@ -21,53 +46,109 @@ class VPRDatasetProcessor: # main ROS class
         self.init_netvlad   = init_netvlad
         self.init_hybridnet = init_hybridnet
 
-        self.print          = printer
+        self.ros            = ros
         self.netvlad        = None
         self.hybridnet      = None
 
-        if self.init_netvlad:
+        if self.init_netvlad: # If needed, initialise NetVLAD
             self.netvlad    = NetVLAD_Container(cuda=self.cuda, ngpus=int(self.cuda), logger=self.print)
 
-        if self.init_hybridnet:
+        if self.init_hybridnet: # If needed, initialise HybridNet
             self.hybridnet  = HybridNet_Container(cuda=self.cuda, logger=self.print)
 
-        if not (dataset_params is None): # None-case needed for SVM training
+        if not (dataset_params is None): # If parameters have been provided:
             self.print("[VPRDatasetProcessor] Loading model from parameters...")
             self.load_dataset(dataset_params, try_gen=try_gen)
 
             if not self.dataset_ready:
                 raise Exception("Dataset load failed.")
             self.print("[VPRDatasetProcessor] Dataset Ready.")
-        else:
+
+        else: # None-case needed for SVM training
             self.print("[VPRDatasetProcessor] Ready; no dataset loaded.")
 
+    def print(self, text, logtype=LogType.INFO, throttle=0):
+        roslogger(text, logtype, throttle=throttle, ros=self.ros)
+
     def prep_netvlad(self, cuda=None, load=True, prep=True):
+        '''
+        Prepare netvlad for use
+
+        Inputs:
+            cuda:   bool type {default: None}; if not None, overrides initialisation cuda variable for netvlad loading
+            load:   bool type {default: True}; whether or not to automatically trigger netvlad.load()
+            prep:   bool type {default: True}; whether or not to automatically trigger netvlad.prep()
+        Returns:
+            None
+        '''
         if not (cuda is None):
-            self.cuda = cuda
-        self.netvlad        = NetVLAD_Container(cuda=self.cuda, ngpus=int(self.cuda), logger=self.print, load=load, prep=prep)
+            cuda = self.cuda
+        self.netvlad        = NetVLAD_Container(cuda=cuda, ngpus=int(self.cuda), logger=lambda x: self.print(x, LogType.DEBUG), load=load, prep=prep)
         self.init_netvlad   = True
 
-    def prep_hybridnet(self, cuda=None):
+    def prep_hybridnet(self, cuda=None, load=True):
+        '''
+        Prepare hybridnet for use
+
+        Inputs:
+            cuda:   bool type {default: None}; if not None, overrides initialisation cuda variable for hybridnet loading
+            load:   bool type {default: True}; whether or not to automatically trigger hybridnet.load()
+        Returns:
+            None
+        '''
         if not (cuda is None):
-            self.cuda = cuda
-        self.hybridnet  = HybridNet_Container(cuda=self.cuda, logger=self.print)
+            cuda = self.cuda
+        self.hybridnet  = HybridNet_Container(cuda=cuda, logger=lambda x: self.print(x, LogType.DEBUG), load=load)
         self.init_hybridnet   = True
 
-    def pass_nns(self, vpr_image_proc, netvlad=True, hybridnet=True):
+    def pass_nns(self, processor, netvlad=True, hybridnet=True):
+        '''
+        Overwrite this VPRDatasetProcessor's instances of netvlad and hybridnet with another's instantiations
+        Will check and, if initialised, destroy existing netvlad/hybridnet
+
+        Inputs:
+            processor:  VPRDatasetProcessor type
+            netvlad:    bool type {default: True}; whether or not to overwrite netvlad instance
+            hybridnet:  bool type {default: True}; whether or not to overwrite hybridnet instance
+        Returns:
+            None
+        '''
         if (not self.netvlad is None) and netvlad:
-            self.netvlad.destroy()
-            self.netvlad = vpr_image_proc.netvlad
+            if isinstance(self.netvlad, NetVLAD_Container):
+                self.netvlad.destroy()
+            self.netvlad = processor.netvlad
             self.init_netvlad = True
         if (not self.hybridnet is None) and hybridnet:
-            self.hybridnet.destroy()
-            self.hybridnet = vpr_image_proc.hybridnet
+            if isinstance(self.hybridnet, HybridNet_Container):
+                self.hybridnet.destroy()
+            self.hybridnet = processor.hybridnet
             self.init_hybridnet = True
 
-    def generate_dataset(self, npz_dbp, bag_dbp, bag_name, sample_rate, odom_topic, img_topics, img_dims, ft_types, filters={}):
+    def generate_dataset(self, npz_dbp, bag_dbp, bag_name, sample_rate, odom_topic, img_topics, img_dims, ft_types, filters={}, store=True):
+        '''
+        Generate new datasets from parameters
+        Inputs are specified such that, other than store, a correct dataset parameters dictionary can be dereferenced to autofill inputs
+
+        Inputs:
+            npz_dbp:        string type
+            bag_dbp:        string type
+            bag_name:       string type
+            sample_rate:    float type
+            odom_topic:     string type
+            img_topics:     string list type
+            img_dims:       int list type
+            ft_types:       string list type
+            filters:        string type
+            store:          bool type; whether or not to overwrite this VPRDatasetProcessor's currently loaded dataset
+        Returns:
+            Generated dataset dictionary
+        '''
+
         # store for access in saving operation:
-        self.dataset_ready  = False
-        self.npz_dbp        = npz_dbp
-        self.bag_dbp        = bag_dbp
+        if store:
+            self.dataset_ready  = False
+            self.npz_dbp        = npz_dbp
+            self.bag_dbp        = bag_dbp
 
         # generate:
         rosbag_dict         = process_bag(rospkg.RosPack().get_path(rospkg.get_package_name(os.path.abspath(__file__))) +  '/' + bag_dbp + '/' + bag_name, sample_rate, odom_topic, img_topics, printer=self.print, use_tqdm=self.use_tqdm)
@@ -84,16 +165,21 @@ class VPRDatasetProcessor: # main ROS class
         dataset_dict.update(feature_vector_dict)
         dataset             = dict(params=params_dict, dataset=dataset_dict)
 
-        del self.dataset   
-        self.dataset        = dataset
-        self.dataset_ready  = True
+        if store:
+            if hasattr(self, 'dataset'):
+                del self.dataset
+            self.dataset        = copy.deepcopy(dataset)
+            self.dataset_ready  = True
 
-        if self.autosave:
-            self.save_dataset()
+            if self.autosave:
+                self.save_dataset()
 
         return dataset
 
     def save_dataset(self, name=None):
+        '''
+        
+        '''
         if not self.dataset_ready:
             raise Exception("Dataset not loaded in system. Either call 'generate_dataset' or 'load_dataset' before using this method.")
         dir = rospkg.RosPack().get_path(rospkg.get_package_name(os.path.abspath(__file__))) + '/' + self.npz_dbp
@@ -140,17 +226,44 @@ class VPRDatasetProcessor: # main ROS class
     def extend_dataset(self, new_ft_type, try_gen=False, save=False):
         if isinstance(new_ft_type, FeatureType):
             new_ft_type = enum_name(new_ft_type)
-        new_params = self.dataset['params']
-        new_params['ft_types'] = new_ft_type
-        if not self.load_dataset(new_params):
-            if try_gen:
-                new_dataset = self.generate_dataset(**new_params, load=False)
-            else:
-                return False
-        self.dataset[new_ft_type] = copy.deepcopy(new_dataset[new_ft_type])
+        new_params = copy.deepcopy(self.dataset['params'])
+        new_params['ft_types'] = [new_ft_type]
+        self.print("[extend_dataset] Loading dataset to extend existing...", LogType.DEBUG)
+        new_dataset = self.open_dataset(new_params, try_gen=try_gen)
+        if new_dataset is None:
+            self.print("[extend_dataset] Load failed.", LogType.DEBUG)
+            return False
+        self.print("[extend_dataset] Load success. Extending.", LogType.DEBUG)
+        self.dataset['dataset'][new_ft_type] = copy.deepcopy(new_dataset['dataset'][new_ft_type])
+        self.dataset['params']['ft_types'].append(new_ft_type)
         if save:
             self.save_dataset()
         return True
+
+    def open_dataset(self, dataset_params, try_gen=False):
+    # load via search for param match but don't load into the processor's self.dataset
+        assert len(dataset_params['ft_types']) == 1, 'Can only open one dataset'
+        assert self.npz_dbp == dataset_params['npz_dbp'], 'Must preserve npz_dbp'
+        assert self.bag_dbp == dataset_params['bag_dbp'], 'Must preserve bag_dbp'
+
+        self.print("[open_dataset] Loading dataset.", LogType.DEBUG)
+        datasets = self._get_datasets()
+        for name in datasets:
+            if datasets[name]['params'] == dataset_params:
+                try:
+                    self.print("[open_dataset] Loading ...", LogType.DEBUG)
+                    return self._load(name, store=False)
+                except:
+                    self.print(formatException())
+        if try_gen:
+            try:
+                self.print("[open_dataset] Generating ...", LogType.DEBUG)
+                return self.generate_dataset(**dataset_params, store=False)
+            except:
+                self.print(formatException())
+        
+        self.print("[open_dataset] Load, generation failed.", LogType.DEBUG)
+        return None
 
     def load_dataset(self, dataset_params, try_gen=False):
     # load via search for param match
@@ -159,14 +272,15 @@ class VPRDatasetProcessor: # main ROS class
         self.dataset_ready = False
         self.print("[load_dataset] Loading dataset.")
         datasets = self._get_datasets()
-        self.dataset = {}
         sub_params = copy.deepcopy(dataset_params)
         sub_params['ft_types'] = [dataset_params['ft_types'][0]]
-
         for name in datasets:
-            if dict(datasets[name]['params']) == sub_params:
-                self._load(name)
-                break
+            if datasets[name]['params'] == sub_params:
+                try:
+                    self._load(name)
+                    break
+                except:
+                    self._fix(name)
         if self.dataset_ready:
             if len(dataset_params['ft_types']) > 1:
                 for ft_type in dataset_params['ft_types'][1:]:
@@ -181,16 +295,18 @@ class VPRDatasetProcessor: # main ROS class
     
     def swap(self, dataset_params, generate=False, allow_false=True):
         # Check if we can just extend the current dataset:
-        dataset_params_test = copy.deepcopy(dataset_params)
-        dataset_params_test['ft_types'] = self.dataset['params']['ft_types']
-        if dataset_params_test == self.dataset['params']:
-            for ft_type in dataset_params['ft_types']:
-                if not (ft_type in self.dataset['params']['ft_types']):
-                    if not self.extend_dataset(ft_type, try_gen=generate, save=generate):
-                        if not allow_false:
-                            raise Exception('Dataset failed to load.')
-                        return False
-            return True
+        try:
+            self.print('[swap] Attempting to extend dataset...')
+            dataset_params_test = copy.deepcopy(dataset_params)
+            dataset_params_test['ft_types'] = copy.copy(self.dataset['params']['ft_types'])
+            if dataset_params_test == self.dataset['params']:
+                self.print("[swap] Candidate for extension. Feature types: %s [against: %s]" % (str(dataset_params['ft_types']), str(self.dataset['params']['ft_types'])), LogType.DEBUG)
+                for ft_type in dataset_params['ft_types']:
+                    if not (ft_type in self.dataset['params']['ft_types']):
+                        return self.extend_dataset(ft_type, try_gen=generate, save=generate)
+        except:
+            self.print("[swap] Dataset corrupted; forcing a fresh dataset load. Error details: " + formatException(), LogType.DEBUG)
+        self.print('[swap] Extension cancelled, attempting to load dataset...')
         # Can't extend, let's load in the replacement:
         if self.load_dataset(dataset_params, try_gen=generate):
             return True
@@ -246,41 +362,33 @@ class VPRDatasetProcessor: # main ROS class
     def _fix(self, dataset_name):
         if not dataset_name.endswith('.npz'):
             dataset_name = dataset_name + '.npz'
-        self.print("[_fix] Bad dataset state detected, performing cleanup...")
+        self.print("[_fix] Bad dataset state detected, performing cleanup...", LogType.DEBUG)
         try:
             os.remove(rospkg.RosPack().get_path(rospkg.get_package_name(os.path.abspath(__file__))) + '/' + self.npz_dbp + '/' + dataset_name)
-            self.print("[_fix] Purged: %s" % (self.npz_dbp + '/' + dataset_name))
+            self.print("[_fix] Purged: %s" % (self.npz_dbp + '/' + dataset_name), LogType.DEBUG)
         except:
             pass
         try:
             os.remove(rospkg.RosPack().get_path(rospkg.get_package_name(os.path.abspath(__file__))) + '/' + self.npz_dbp + '/params/' + dataset_name)
-            self.print("[_fix] Purged: %s" % (self.npz_dbp + '/params/' + dataset_name))
+            self.print("[_fix] Purged: %s" % (self.npz_dbp + '/params/' + dataset_name), LogType.DEBUG)
         except:
             pass
 
-    def _load(self, dataset_name,):
+    def _load(self, dataset_name, store=True):
     # when loading objects inside dicts from .npz files, must extract with .item() each object
         if not dataset_name.endswith('.npz'):
             dataset_name = dataset_name + '.npz'
-        full_file_path = rospkg.RosPack().get_path(rospkg.get_package_name(os.path.abspath(__file__))) + '/' + self.npz_dbp + '/' + dataset_name
-        self.print('[_load] Attempting to load: %s' % full_file_path)
-        raw_dataset = np.load(full_file_path, allow_pickle=True)
+        full_file_path  = rospkg.RosPack().get_path(rospkg.get_package_name(os.path.abspath(__file__))) + '/' + self.npz_dbp + '/' + dataset_name
 
-        del self.dataset
-        self.dataset = dict(dataset=raw_dataset['dataset'].item(), params=raw_dataset['params'].item())
-        self.dataset_ready = True
-        return self.dataset
-    
-    def _extend(self, dataset_name, ft_type):
-    # when loading objects inside dicts from .npz files, must extract with .item() each object
-        if not dataset_name.endswith('.npz'):
-            dataset_name = dataset_name + '.npz'
-        if isinstance(ft_type, FeatureType):
-            ft_type = enum_name(ft_type)
-        
-        raw_dataset = np.load(rospkg.RosPack().get_path(rospkg.get_package_name(os.path.abspath(__file__))) + '/' + self.npz_dbp + "/" + dataset_name, allow_pickle=True)['dataset'].item()
-        self.dataset['dataset'][ft_type] = raw_dataset[ft_type]
-        return self.dataset
+        self.print('[_load] Attempting to load: %s' % full_file_path, LogType.DEBUG)
+        raw_dataset     = np.load(full_file_path, allow_pickle=True)
+        dataset         = dict(dataset=raw_dataset['dataset'].item(), params=raw_dataset['params'].item())
+        if store:
+            if hasattr(self, 'dataset'):
+                del self.dataset
+            self.dataset = copy.deepcopy(dataset)
+            self.dataset_ready = True
+        return dataset
     
     def destroy(self):
         if self.init_hybridnet:
