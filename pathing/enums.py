e0736d59e6a04e6844caf6045a7d552123b09ac0
Owen Claxton <claxtono@qut.edu.au>
2023-09-22 12:35:38 +1000

    New Enums: - PS4_Pressed: Pass in a Joy message to check if active - PS4_Triggers -> PS4_Axes - PS4_Axes, PS4_Buttons now mapped correctly - Experiment_Mode: Control flow of experiment execution - Point_Shoot_Stage: Control flow of a point-and-shoot target movement - Technique: For experiments - Save_Request: Trigger waypoint saving in path_follower - HDi: indexing for historical data storage in path_follower

--- pathing/enums.py
+++ pathing/enums.py
@@ -1,5 +1,24 @@
 from enum import Enum
 
+class PS4_Pressed(Enum):
+    X               = lambda msg: msg.buttons[  PS4_Buttons.X.value             ] > 0
+    O               = lambda msg: msg.buttons[  PS4_Buttons.O.value             ] > 0
+    Triangle        = lambda msg: msg.buttons[  PS4_Buttons.Triangle.value      ] > 0
+    Square          = lambda msg: msg.buttons[  PS4_Buttons.Square.value        ] > 0
+    LeftBumper      = lambda msg: msg.buttons[  PS4_Buttons.LeftBumper.value    ] > 0
+    RightBumper     = lambda msg: msg.buttons[  PS4_Buttons.RightBumper.value   ] > 0
+    LeftTrigger     = lambda msg: msg.buttons[  PS4_Buttons.LeftTrigger.value   ] > 0
+    RightTrigger    = lambda msg: msg.buttons[  PS4_Buttons.RightTrigger.value  ] > 0
+    Share           = lambda msg: msg.buttons[  PS4_Buttons.Share.value         ] > 0
+    Options         = lambda msg: msg.buttons[  PS4_Buttons.Options.value       ] > 0
+    PS              = lambda msg: msg.buttons[  PS4_Buttons.PS.value            ] > 0
+    LeftStickIn     = lambda msg: msg.buttons[  PS4_Buttons.LeftStickIn.value   ] > 0
+    RightStickIn    = lambda msg: msg.buttons[  PS4_Buttons.RightStickIn.value  ] > 0
+    LeftArrow       = lambda msg: msg.axes[     PS4_Axes.ArrowsXAxis.value      ] > 0.5
+    RightArrow      = lambda msg: msg.axes[     PS4_Axes.ArrowsXAxis.value      ] < -0.5
+    UpArrow         = lambda msg: msg.axes[     PS4_Axes.ArrowsYAxis.value      ] > 0.5
+    DownArrow       = lambda msg: msg.axes[     PS4_Axes.ArrowsYAxis.value      ] < -0.5
+
 class PS4_Buttons(Enum):
     X               = 0
     O               = 1
@@ -7,23 +26,23 @@ class PS4_Buttons(Enum):
     Square          = 3
     LeftBumper      = 4
     RightBumper     = 5
-    Share           = 6
-    Options         = 7
-    PS              = 8
-    LeftStickIn     = 9
-    RightStickIn    = 10
-    LeftArrow       = 11
-    RightArrow      = 12
-    UpArrow         = 13
-    DownArrow       = 14
+    LeftTrigger     = 6
+    RightTrigger    = 7
+    Share           = 8
+    Options         = 9
+    PS              = 10
+    LeftStickIn     = 11
+    RightStickIn    = 12
 
-class PS4_Triggers(Enum):
-    LeftStickXAxis  = 0
-    LeftStickYAxis  = 1
+class PS4_Axes(Enum):
+    LeftStickXAxis  = 0 # Left = 1, Right = -1
+    LeftStickYAxis  = 1 # Up = 1, Down = -1
     LeftTrigger     = 2 # Released = 1, Pressed = -1
-    RightStickXAxis = 3
-    RightStickYAxis = 4
+    RightStickXAxis = 3 # Left = 1, Right = -1
+    RightStickYAxis = 4 # Up = 1, Down = -1
     RightTrigger    = 5 # Released = 1, Pressed = -1
+    ArrowsXAxis     = 6 # Left = 1, Right = -1
+    ArrowsYAxis     = 7 # Up = 1, Down = -1
 
 class Safety_Mode(Enum):
     UNSET           = -1
@@ -36,6 +55,17 @@ class Command_Mode(Enum):
     VPR             = 1
     SLAM            = 2
     ZONE_RETURN     = 3
+    SPECIAL         = 4
+
+class Experiment_Mode(Enum):
+    UNSET           = 0
+    INIT            = 1
+    ALIGN           = 2
+    DRIVE_PATH      = 3
+    HALT1           = 4
+    DRIVE_GOAL      = 5
+    HALT2           = 6
+    DONE            = 7
 
 class Lookahead_Mode(Enum):
     INDEX           = 0
@@ -47,9 +77,42 @@ class Return_Stage(Enum):
     TURN            = 2
     DONE            = 3
 
+class Point_Shoot_Stage(Enum):
+    UNSET           = 0
+    INIT            = 1
+    POINT           = 2
+    SHOOT           = 3
+    DONE            = 4
+
 class Reject_Mode(Enum):
     NONE            = 0
     STOP            = 1
     OLD             = 2
     OLD_50          = 150
     OLD_90          = 190
+
+class Technique(Enum):
+    VPR             = 0
+    SVM             = 1
+
+class Save_Request(Enum):
+    NONE            = 0
+    CLEAR           = 1
+    SET             = 2
+
+class HDi(Enum):
+    mInd            = 0     # match index
+    tInd            = 1     # true index
+    mDist           = 2     # match distance
+    gt_class        = 3     # ground truth classification
+    svm_class       = 4     # svm classification
+    slam_x          = 5     # SLAM pose x
+    slam_y          = 6     # SLAM pose y
+    slam_w          = 7     # SLAM pose w
+    robot_x         = 8     # robot wheel encoder pose x
+    robot_y         = 9     # robot wheel encoder pose y
+    robot_w         = 10    # robot wheel encoder pose w
+    vpr_x           = 11    # VPR pose x
+    vpr_y           = 12    # VPR pose y
+    vpr_w           = 13    # VPR pose w
+    dist            = 14    # distance since last point
