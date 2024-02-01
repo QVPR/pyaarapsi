de4c744b0ae22982cb3891a3103a57cdff29d5b0
Owen Claxton <claxtono@qut.edu.au>
2024-02-01 13:24:47 +1000

    Upgraded download models: - replaced gdown with custom bash script to pull files from google drive. More reliable - Added SALAD model download function, download_salad_models - Added SALAD model download to download_all_models

--- vpr_classes/download_models.py
+++ vpr_classes/download_models.py
@@ -1,18 +1,21 @@
 #!/usr/bin/env python3
 
 import os
-import gdown
+import subprocess
 import sys
 import os
 import imp # TODO: deprecated :-(
 MODELS_DIR = imp.find_module("pyaarapsi")[1] + '/vpr_classes/downloads/'
 from ..core.helper_tools import ask_yesnoexit
 
+def download_file(id, file_name):
+    subprocess.run([MODELS_DIR + "downloader.sh", id, MODELS_DIR + file_name])
+
 def download_netvlad_models(force=False):
     global MODELS_DIR
     dest_dir = os.path.join(MODELS_DIR)
     print("Path to netvlad model/s:\n\t<%s>" % dest_dir)
-    if ask_yesnoexit("Auto-download pretrained NetVLAD model/s (takes around 2GB of space)? (y)es/(n)o/(q)uit."):
+    if ask_yesnoexit("Auto-download pretrained NetVLAD model/s (takes around 2GB of space)? (y)es/(n)o/(q)uit.", auto=5):
         if force:
             try:
                 os.remove(os.path.join(dest_dir, "mapillary_WPCA128.pth.tar"))
@@ -20,8 +23,8 @@ def download_netvlad_models(force=False):
                 pass
         if not os.path.isfile(os.path.join(dest_dir, "mapillary_WPCA128.pth.tar")):
             print('Downloading mapillary_WPCA128.pth.tar')
-            gdown.download("https://drive.google.com/uc?id=1v-MDvga__tblvZ9-m4Zfk6iem4c4Gjyh", os.path.join(dest_dir, "mapillary_WPCA128.pth.tar"), quiet=False)
-        
+            download_file("1v-MDvga__tblvZ9-m4Zfk6iem4c4Gjyh", "mapillary_WPCA128.pth.tar")
+
         if force:
             try:
                 os.remove(os.path.join(dest_dir, "mapillary_WPCA512.pth.tar"))
@@ -29,7 +32,7 @@ def download_netvlad_models(force=False):
                 pass
         if not os.path.isfile(os.path.join(dest_dir, "mapillary_WPCA512.pth.tar")):
             print('Downloading mapillary_WPCA512.pth.tar')
-            gdown.download("https://drive.google.com/uc?id=1D9V_k7VoTCbq6L0B8hVIqniV1vGFpCB8", os.path.join(dest_dir, "mapillary_WPCA512.pth.tar"), quiet=False)
+            download_file("1D9V_k7VoTCbq6L0B8hVIqniV1vGFpCB8", "mapillary_WPCA512.pth.tar")
         
         if force:
             try:
@@ -38,7 +41,7 @@ def download_netvlad_models(force=False):
                 pass
         if not os.path.isfile(os.path.join(dest_dir, "mapillary_WPCA4096.pth.tar")):
             print('Downloading mapillary_WPCA4096.pth.tar')
-            gdown.download("https://drive.google.com/uc?id=14fe_v2OYVJDa8qCiG1gcgf9I81qFRCGv", os.path.join(dest_dir, "mapillary_WPCA4096.pth.tar"), quiet=False)
+            download_file("14fe_v2OYVJDa8qCiG1gcgf9I81qFRCGv", "mapillary_WPCA4096.pth.tar")
        
         if force:
             try:
@@ -47,7 +50,7 @@ def download_netvlad_models(force=False):
                 pass
         if not os.path.isfile(os.path.join(dest_dir, "pittsburgh_WPCA128.pth.tar")):
             print('Downloading pittsburgh_WPCA128.pth.tar')
-            gdown.download("https://drive.google.com/uc?id=1pnqUoREiTk-UJzU4N4n5doUm4dOIPxIU", os.path.join(dest_dir, "pittsburgh_WPCA128.pth.tar"), quiet=False)
+            download_file("1pnqUoREiTk-UJzU4N4n5doUm4dOIPxIU", "pittsburgh_WPCA128.pth.tar")
         
         if force:
             try:
@@ -56,7 +59,7 @@ def download_netvlad_models(force=False):
                 pass
         if not os.path.isfile(os.path.join(dest_dir, "pittsburgh_WPCA512.pth.tar")):
             print('Downloading pittsburgh_WPCA512.pth.tar')
-            gdown.download("https://drive.google.com/uc?id=1FGk77XsRo5ZRHHaxcCSmH_rhOdl0ByFs", os.path.join(dest_dir, "pittsburgh_WPCA512.pth.tar"), quiet=False)
+            download_file("1FGk77XsRo5ZRHHaxcCSmH_rhOdl0ByFs", "pittsburgh_WPCA512.pth.tar")
        
         if force:
             try:
@@ -65,14 +68,14 @@ def download_netvlad_models(force=False):
                 pass
         if not os.path.isfile(os.path.join(dest_dir, "pittsburgh_WPCA4096.pth.tar")):
             print('Downloading pittsburgh_WPCA4096.pth.tar')
-            gdown.download("https://drive.google.com/uc?id=1TcF6Z2n7lxkf_9pXnEbJXmOzCMEljhjO", os.path.join(dest_dir, "pittsburgh_WPCA4096.pth.tar"), quiet=False)
+            download_file("1TcF6Z2n7lxkf_9pXnEbJXmOzCMEljhjO", "pittsburgh_WPCA4096.pth.tar")
         print('Downloaded all pretrained models.')
 
 def download_hybridnet_models(force=False):
     global MODELS_DIR
     dest_dir = os.path.join(MODELS_DIR)
     print("Path to hybridnet model/s:\n\t<%s>" % dest_dir)
-    if ask_yesnoexit("Auto-download pretrained HybridNet model/s (takes around 300MB of space)? (y)es/(n)o/(q)uit."):
+    if ask_yesnoexit("Auto-download pretrained HybridNet model/s (takes around 300MB of space)? (y)es/(n)o/(q)uit.", auto=5):
         
         if force:
             try:
@@ -81,13 +84,28 @@ def download_hybridnet_models(force=False):
                 pass
         if not os.path.isfile(os.path.join(dest_dir, "HybridNet.caffemodel")):
             print('Downloading HybridNet.caffemodel')
-            gdown.download("https://drive.google.com/uc?id=1_tzdDkM5TeyB37TEduyxWMi_T4CqQ4u4", os.path.join(dest_dir, "HybridNet.caffemodel"), quiet=False)
+            download_file("1_tzdDkM5TeyB37TEduyxWMi_T4CqQ4u4", "HybridNet.caffemodel")
+
+def download_salad_models(force=False):
+    global MODELS_DIR
+    dest_dir = os.path.join(MODELS_DIR)
+    print("Path to salad model/s:\n\t<%s>" % dest_dir)
+    if ask_yesnoexit("Auto-download pretrained salad model/s (takes around 330MB of space)? (y)es/(n)o/(q)uit.", auto=5):
+        
+        if force:
+            try:
+                os.remove(os.path.join(dest_dir, "dino_salad.ckpt"))
+            except:
+                pass
+        if not os.path.isfile(os.path.join(dest_dir, "dino_salad.ckpt")):
+            print('Downloading dino_salad.ckpt')
+            download_file("1MH7vvvXmbjwMlU_bNx4CR4iFpu_fRYcY", "dino_salad.ckpt")
 
 def download_all_models(force=False):
     
     download_netvlad_models(force)
-    print("Path to hybridnet model/s:\n\t<%s>" % os.path.join(MODELS_DIR))
     download_hybridnet_models(force)
+    download_salad_models(force)
 
 if __name__ == "__main__":
     download_all_models()
