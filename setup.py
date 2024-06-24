909394b218c5227e0e96e5592e3f6a66119ab414
Owen Claxton <claxtono@qut.edu.au>
2024-06-24 12:11:22 +1000

    0.2.2

--- setup.py
+++ setup.py
@@ -62,7 +62,7 @@ ros_install_versioned_require_list = [
             
 setup(
     name='pyaarapsi',
-    version='0.2.1',
+    version='0.2.2',
     description='PyAARAPSI: Python Package for AARAPSI Project',
     author='Owen Claxton, Connor Malone, and Helen Carson',
     author_email='claxtono@qut.edu.au',
